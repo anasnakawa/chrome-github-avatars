43a54cb00a77bec8adac10ab0896f9587e197e0e
Anas Nakawa <anas.nakawa@gmail.com>
2016-01-18 18:16:22 +0200

    removed analytics

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -113,15 +113,3 @@ $news.on('click', buttonSelector, function() {
         
     }, 500);
 });
-
-// analytics
-// ---------
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-72567125-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
